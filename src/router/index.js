11c062ac0252bae8710d32597a23d18c7bed72ef
Dany Sentiana <danysentiana1200@gmail.com>
2023-08-17 22:06:17 +0700

    configure vue-router and add some router

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,39 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home from '@/views/HomeView.vue'
+import About from '@/views/AboutView.vue'
+import Manage from '@/views/ManageView.vue'
+
+const routes = [
+  {
+    name: 'home',
+    path: '/',
+    component: Home
+  },
+  {
+    name: 'about',
+    path: '/about',
+    component: About
+  },
+  {
+    name: 'manage',
+    path: '/manage',
+    component: Manage
+  },
+  {
+    // old route
+    path: '/manage-music',
+    redirect: { name: 'manage' }
+  },
+  {
+    path: '/:catchAll(.*)*',
+    redirect: { name: 'home' }
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+  routes,
+  linkExactActiveClass: 'text-yellow-500'
 })
 
 export default router
