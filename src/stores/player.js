2c67e137331120ed3c3b6b57e6526b48366097e9
Dany Sentiana <danysentiana1200@gmail.com>
2023-08-23 19:47:00 +0700

    add MuscPlayer component and player state, implement playing, pause, and resume music

--- src/stores/player.js
+++ src/stores/player.js
@@ -4,18 +4,38 @@ import { Howl } from 'howler'
 export default defineStore('player', {
   state: () => ({
     current_song: {},
-    sounds: {}
+    sound: {}
   }),
+  getters: {
+    playing: (state) => {
+      if (state.sound.playing) {
+        return state.sound.playing()
+      }
+
+      return false
+    }
+  },
   actions: {
     async newSong(song) {
       this.current_song = song
 
-      this.sounds = new Howl({
+      this.sound = new Howl({
         src: [song.url],
         html5: true
       })
 
-      this.sounds.play()
+      this.sound.play()
+    },
+    async toggleAudio() {
+      if (!this.sound.playing) {
+        return
+      }
+
+      if (this.sound.playing()) {
+        this.sound.pause()
+      } else {
+        this.sound.play()
+      }
     }
   }
 })
