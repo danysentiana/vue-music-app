cdeed6aeff99ddebd1fda7254c911b1466bfb0f5
Dany Sentiana <danysentiana1200@gmail.com>
2023-08-24 20:47:17 +0700

    add route fade transition to app

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <app-header></app-header>
 
-  <router-view></router-view>
+  <router-view v-slot="{ Component }">
+    <transition name="fade" mode="out-in">
+      <component :is="Component"></component>
+    </transition>
+  </router-view>
 
   <!-- Player -->
   <music-player></music-player>
@@ -34,3 +38,18 @@ export default {
   }
 }
 </script>
+
+<style>
+.fade-enter-form {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: all 0.5s linear;
+}
+
+.fade-leave-to {
+  transition: all 0.5s linear;
+  opacity: 0;
+}
+</style>
