60b134b8bf74714f68679e59b6ab8da150460fad
Dany Sentiana <danysentiana1200@gmail.com>
2023-08-25 20:35:52 +0700

    add internalization i18n

---
+++ src/includes/i18n.js
@@ -0,0 +1,23 @@
+import { createI18n } from 'vue-i18n'
+import en from '@/locales/en.json'
+import id from '@/locales/id.json'
+
+export default createI18n({
+  locale: 'en',
+  fallbackLocale: 'en',
+  messages: { en, id },
+  numberFormats: {
+    en: {
+      currency: {
+        style: 'currency',
+        currency: 'USD'
+      }
+    },
+    id: {
+      currency: {
+        style: 'currency',
+        currency: 'IDR'
+      }
+    }
+  }
+})
